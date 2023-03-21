17a5cf334638b83d6c9999f6bede210c7d7da89a
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-03-21 21:16:32 +0000

    Make site work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,71 @@
+import React from "react";
 import "./App.css";
+import { useState, useEffect } from "react";
+import Post from "./components/Post";
+import { getPost } from "./backend/backend";
+import ErrorDlg from "./components/ErrorDlg";
 
 function App() {
-  return <div className="App"></div>;
+  const POST_ID = "63dbaf9412e514c68d95c4ba";
+
+  const [post, setPost] = useState({
+    title: "...Loading",
+    author: {
+      username: "...Loading",
+    },
+    content: [],
+  });
+
+  const [errors, setErrors] = useState([]);
+
+  const [open, setOpen] = useState(false);
+
+  function close() {
+    setOpen(false);
+  }
+
+  async function fetchPost(id) {
+    try {
+      const response = await getPost(id);
+      const { success, post, errors } = response;
+      if (!success) {
+        setErrors(errors);
+      } else {
+        setPost(post);
+      }
+    } catch (err) {
+      setErrors([{ msg: err.message }]);
+    }
+  }
+
+  useEffect(() => {
+    fetchPost(POST_ID);
+  }, []);
+
+  useEffect(() => {
+    if (errors.length === 0) return;
+    setOpen(true);
+  }, [errors]);
+
+  return (
+    <div className="App">
+      <Post
+        post={post}
+        submit={() => {}}
+        updateBlock={() => {}}
+        errors={[]}
+        setPost={() => {}}
+        token={""}
+        edit={false}
+      />
+
+      <ErrorDlg
+        open={open}
+        close={close}
+        error={errors.length > 0 ? errors[0] : { msg: "" }}
+      />
+    </div>
+  );
 }
 
 export default App;
